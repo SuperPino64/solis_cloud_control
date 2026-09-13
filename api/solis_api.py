390ba2ad2656263bfa265504fbc1b5190a8c50d8
SuperPino <github@bouterbv.eu>
2026-09-13 18:22:41 +0200

    Increase timeout from 30 to 60 seconds

--- api/solis_api.py
+++ api/solis_api.py
@@ -40,7 +40,7 @@ class SolisCloudControlApiClient:
     _CONTROL_ENDPOINT = "/v2/api/control"
     _INVERTER_LIST_ENDPOINT = "/v1/api/inverterList"
     _INVERTER_DETAILS_ENDPOINT = "/v1/api/inverterDetail"
-    _TIMEOUT_SECONDS = 30
+    _TIMEOUT_SECONDS = 60
     _CONCURRENT_REQUESTS = 1
     _MAX_RETRY_TIME_SECONDS = 30
 
